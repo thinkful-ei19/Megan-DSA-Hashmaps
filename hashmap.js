ac6be7a72a123c4f35a2953863d8738cb952b39a
leonardom249 <leonardo.megan@gmail.com>
2018-05-21 14:35:07 -0500

    switched out palindrome formula

--- hashmap.js
+++ hashmap.js
@@ -5,6 +5,7 @@ class HashMap {
         this.length = 0;
         this._slots = [];
         this._capacity = initialCapacity;
+        this._deleted=0;
     }
 
     get(key){
@@ -15,28 +16,23 @@ class HashMap {
         return this._slots[index].value;
     }
 
-    set(key, value){
-        const loadRatio=(this.length+1)/this._capacity;
-        if(loadRatio>HashMap.MAX_LOAD_RATIO){
-            this._resize(this._capacity*HashMap.SIZE_RATIO);
+    set(key, value) {
+        const loadRatio = (this.length + 1) / this._capacity;
+        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+          this._resize(this._capacity * HashMap.SIZE_RATIO);
         }
         const index = this._findSlot(key);
-        
-        //if key exists change value but dont change length
-        //if not add value and change length
-        if(!this._slots[index]){
-            this._slots[index]={
-                key, value
-            };
-            this.length++
+    
+        if (!this._slots[index]) {
+          this.length++;
         }
-        else{
-            this._slots[index]={
-                key, value
-            };  
-        }
-        
-    }
+    
+        this._slots[index] = {
+          key,
+          value,
+          deleted: false
+        };
+      }
 
     remove(key){
         const index=this._findSlot(key);
@@ -51,14 +47,13 @@ class HashMap {
 
     _findSlot(key){
         const hash= HashMap._hashString(key);
-        //can we use this here instead of HashMap.?
         const start= hash%this._capacity;
 
         for(let i=start; i<start +this._capacity; i++){
             const index=i% this._capacity;
             const slot= this._slots[index];
 
-            if(slot===undefined||slot.key==key){
+            if(slot===undefined||slot.key==key && !slot.delete){
                 return index;
             }
         }
@@ -71,7 +66,7 @@ class HashMap {
         this._slots=[];
 
         for(const slot of oldSlots){
-            if(slot!==undefined){
+            if(slot!==undefined&& !slot.deleted){
                 this.set(slot.key, slot.value);
             }
         }
@@ -98,15 +93,15 @@ function createLor(){
 
     lor.set("Hobbit", "Bilbo");
     lor.set("Hobbit", "Frodo");
-    // lor.set("Wizard", "Gandolf");
-    // lor.set("Human", "Aragorn");
-    // lor.set("Elf", "Legolas");
-    // lor.set("Maiar", "The Necromancer");
-    // lor.set("Maiar", "Sauron");
-    // lor.set("Ringbearer", "Gollum");
-    // lor.set("LadyOfLight", "Galadriel");
-    // lor.set("HalfElven", "Arwen");
-    // lor.set("Ent", "Treebeard");
+    lor.set("Wizard", "Gandolf");
+    lor.set("Human", "Aragorn");
+    lor.set("Elf", "Legolas");
+    lor.set("Maiar", "The Necromancer");
+    lor.set("Maiar", "Sauron");
+    lor.set("Ringbearer", "Gollum");
+    lor.set("LadyOfLight", "Galadriel");
+    lor.set("HalfElven", "Arwen");
+    lor.set("Ent", "Treebeard");
 
 //    return lor.get('Maiar')
 
@@ -116,68 +111,43 @@ return lor;
 
 console.log(createLor());
 
-// function palindromeChecker(str, permutation){
-//     if (str.length !== permutation.length){
-//         return false;
-//     }
+function palindromeChecker2(str){
+    let palMap= new HashMap();
+    let oddKeys= new HashMap();
+    let middleLetterCount=0;
+    let numOfOddLetters=0;
 
-//     const strMap = new HashMap();
-//     const palMap = new HashMap();
+    for(let i=0; i<str.length; i++){
+        const index = palMap._findSlot(str[i]);
 
-//     for(let i=0; i<str.length; i++){
-//         let count=0;
-//         strMap.set(str[i], count);
+         if(!palMap._slots[index]){
+             palMap.set(str[i], 1);
+         }
+         else{
+             let value = palMap.get(str[i]);
+             palMap.set(str[i], value+1)
+         }
+    }
 
-//         for(let j=0; j<str.length; j++){
-//            if(str[i]===str[j]){
-//                let value = strMap.get(str[i]);
-//                strMap.set(str[i], value+1);
-//            }
-//         }   
-//     }
+    for(let i=0; i<str.length; i++){
+        const value= palMap.get(str[i])
 
-//     for(let i=0; i<permutation.length; i++){
-//         let count=0;
-//         palMap.set(permutation[i], count);
-
-//         for(let j=0; j<permutation.length; j++){
-
-//            if(permutation[i]===permutation[j]){
-//                let value = palMap.get(permutation[i]);
-//                palMap.set(permutation[i], value+1);
-//            }
-//         }   
-//     }
-
-//     for(let i=0; i<str.length; i++){
-//         let strKey = str[i];
-//         let strValue= strMap.get(strKey);
-       
-//         for(let j=0; j<permutation.length; j++){
-//            let palKey= permutation[j];
-//            let palValue = palMap.get(palKey);
-//            try{
-//                strMap.get(palKey)
-//             }
-//            catch(err){
-//                if(err.message==='Key Error'){
-//                    return false;
-//                }
-//            }
-
-//            if(strKey===palKey){
-//                if(strValue!==palValue){
-//                    return false;
-//                }
-//            }
-          
-//         }   
-//     }
+        if (value%2 === 0){
+            continue;
+        }
+        else{
+            oddKeys.set(str[i], value)
+        }
+    }
 
 
-//     return true;
-
-// }
-// console.log(palindromeChecker('racecar', 'carerace'));
-
-// function palindromeChecker2(str)
+    if(oddKeys.length<2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(palindromeChecker2('eracecare'));
+console.log(palindromeChecker2('racecar'));
+console.log(palindromeChecker2('north'));
