fac7b216639bcc21c5fc928524e474102df0a8a6
leonardom249 <leonardo.megan@gmail.com>
2018-05-21 12:29:56 -0500

    completed first challenge

--- hashmap.js
+++ hashmap.js
@@ -18,7 +18,7 @@ class HashMap {
     set(key, value){
         const loadRatio=(this.length+1)/this._capacity;
         if(loadRatio>HashMap.MAX_LOAD_RATIO){
-            this.resize(this._capacity*HashMap.SIZE_RATIO);
+            this._resize(this._capacity*HashMap.SIZE_RATIO);
         }
         const index = this._findSlot(key);
         this._slots[index]={
@@ -81,3 +81,90 @@ class HashMap {
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+function createLor(){
+    const lor = new HashMap();
+
+    lor.set("Hobbit", "Bilbo");
+    lor.set("Hobbit", "Frodo");
+    lor.set("Wizard", "Gandolf");
+    lor.set("Human", "Aragorn");
+    lor.set("Elf", "Legolas");
+    lor.set("Maiar", "The Necromancer");
+    lor.set("Maiar", "Sauron");
+    lor.set("Ringbearer", "Gollum");
+    lor.set("LadyOfLight", "Galadriel");
+    lor.set("HalfElven", "Arwen");
+    lor.set("Ent", "Treebeard");
+
+//    return lor.get('Maiar')
+
+return lor;
+
+}
+
+// console.log(createLor());
+
+function palindromeChecker(str, permutation){
+    if (str.length !== permutation.length){
+        return false;
+    }
+
+    const strMap = new HashMap();
+    const palMap = new HashMap();
+
+    for(let i=0; i<str.length; i++){
+        let count=0;
+        strMap.set(str[i], count);
+
+        for(let j=0; j<str.length; j++){
+           if(str[i]===str[j]){
+               let value = strMap.get(str[i]);
+               strMap.set(str[i], value+1);
+           }
+        }   
+    }
+
+    for(let i=0; i<permutation.length; i++){
+        let count=0;
+        palMap.set(permutation[i], count);
+
+        for(let j=0; j<permutation.length; j++){
+
+           if(permutation[i]===permutation[j]){
+               let value = palMap.get(permutation[i]);
+               palMap.set(permutation[i], value+1);
+           }
+        }   
+    }
+
+    for(let i=0; i<str.length; i++){
+        let strKey = str[i];
+        let strValue= strMap.get(strKey);
+       
+        for(let j=0; j<permutation.length; j++){
+           let palKey= permutation[j];
+           let palValue = palMap.get(palKey);
+           try{
+               strMap.get(palKey)
+            }
+           catch(err){
+               if(err.message==='Key Error'){
+                   return false;
+               }
+           }
+
+           if(strKey===palKey){
+               if(strValue!==palValue){
+                   return false;
+               }
+           }
+          
+        }   
+    }
+
+
+    return true;
+
+}
+console.log(palindromeChecker('racecar', 'carerace'));
